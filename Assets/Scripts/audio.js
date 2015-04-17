db90724aa3c3d8b32bda9de62e944065391665e4
Ben Ackland <ben@netsight.co.uk>
2015-04-17 15:56:47 +0100

    added audio test scene and wav versions of first and last audio files plus background sound

---
+++ Assets/Scripts/audio.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var welcomeAudio : AudioClip;
+
+function Start () {
+	Debug.Log("Start");
+	audio.PlayOneShot(welcomeAudio);
+}
+
+function Update () {
+//	Debug.Log("Update");
+}
+
+@script RequireComponent(AudioSource)
---
+++ Assets/Scripts/playaudio.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var collisionAudio: AudioClip;
+
+ function OnTriggerEnter(trigger:Collider) {
+ 	Debug.Log("Collision");
+ 	audio.PlayOneShot(collisionAudio);
+ }
+ 
+ @script RequireComponent(AudioSource)
