45e5f7e90aef5c3b9d1ed5c5dda644b5532884fe
Ben Ackland <ben@netsight.co.uk>
2015-04-24 13:32:55 +0100

    upon collide with lava2, stop all audio and play the "no match for me" audio

---
+++ Assets/Scripts/collision_audio.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var noMatchForMeAudio: AudioClip;
+
+ private var allAudioSources : AudioSource[];
+  
+ function Awake() {
+     allAudioSources = FindObjectsOfType(AudioSource) as AudioSource[];
+ }
+  
+ function StopAllAudio() {
+     for(var audioS : AudioSource in allAudioSources) {
+         audioS.Stop();
+     }
+ }
+ 
+ function OnTriggerEnter(trigger:Collider) {
+ 	Debug.Log("Collision");
+ 	StopAllAudio();
+ 	GetComponent.<AudioSource>().PlayOneShot(noMatchForMeAudio);
+ }
+ 
+ @script RequireComponent(AudioSource)
